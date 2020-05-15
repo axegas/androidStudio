d68c604c1f684a156d03f42a51627a874654c367
axegas <axegas@alu.iesserpis.org>
2020-05-15 19:28:49 +0200

    d

--- pruebaTab/app/src/main/java/com/example/pruebatab/DatePickerFragment.java
+++ pruebaTab/app/src/main/java/com/example/pruebatab/DatePickerFragment.java
@@ -3,7 +3,6 @@ package com.example.pruebatab;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.widget.DatePicker;
 
 import androidx.fragment.app.DialogFragment;
 
